aab16def5505524e984d1dc2763406f71012eb0f
ahpehgit <ahpeh@hotmail.com>
2025-03-13 15:26:13 +0800

    Update assignment.test.js

--- src/tests/assignment.test.js
+++ src/tests/assignment.test.js
@@ -93,14 +93,14 @@ jest.setTimeout(5000);
 it('Login with correct credentials', async() => {
     //expect(await Login(authorisedUser, authorisedPassword)).toEqual(expect.not.stringContaining('Unauthorized'));
     let res = '';
-    res = await new Promise((resolve) => setTimeout(async() => { resolve(await Login(authorisedUser, authorisedPassword)); }, 500));
-    expect(res).not.toEqual(401);
+    await new Promise((resolve) => setTimeout(async() => {res = await Login(authorisedUser, authorisedPassword); resolve();}, 500));
+    expect(res).not.toBe(401);
 });
 
 it('Login with wrong credentials', async() => {
     let res = '';
     await new Promise((resolve) => setTimeout(async() => {res = await Login(authorisedUser, 'wrongpassword'); resolve();}, 500));
-    expect(res).toEqual(401);
+    expect(res).toBe(401);
 });
 
 it('Get by attraction id A0002', async() => {
